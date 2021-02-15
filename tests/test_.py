c62a9c25c482977711606e7b49278ccc09667936
Yizhu ZONG <agnes.lain@gmail.com>
2021-02-15 17:02:16 +0100

    add test for factorial 1

--- tests/test_.py
+++ tests/test_.py
@@ -3,6 +3,9 @@ from toolkit.lib import factorial, collatz
 def test_factorial():
     assert factorial(4) == 24
 
+def test_factorial_one():
+    assert factorial(1) == 1
+
 def test_collatz_even():
     assert collatz(4) == 2
 
